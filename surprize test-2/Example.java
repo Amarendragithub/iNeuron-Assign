28238e27460ac637ff7ac238c3d8d82981696f1e
Amarendra sahoo <98246165+Amarendragithub@users.noreply.github.com>
2023-06-04 16:52:44 +0530

    Add files via upload

---
+++ surprize test-2/Example.java
@@ -0,0 +1,9 @@
+public class Example {
+    public static void main(String[] args) {
+        String s = "leetcode";
+        Example example = new Example();
+        int index = example.firstUniqChar(s);
+        System.out.println(index);  // Output: 0
+    }
+    
+}
